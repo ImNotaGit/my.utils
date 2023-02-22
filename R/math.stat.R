a4741e39a3c03d367f6159cc4632e441831e8a30
Kuoyuan Cheng <cheng.ky@hotmail.com>
2023-02-22 03:27:02 -0500

    add a copy of limma::makeContrasts

--- R/math.stat.R
+++ R/math.stat.R
@@ -720,3 +720,74 @@ simple.nested.model.matrix <- function(dat, a, b) {
   attr(mat, "contrasts") <- tmp
   mat
 }
+
+
+makeContrasts <- function(..., contrasts=NULL, levels, check.names=TRUE) {
+  #	Construct matrix of custom contrasts
+  #	Gordon Smyth
+  #	30 June 2003.  Last modified 2 April 2010.
+  # this is a copy of limma::makeContrasts from version 3.54.1
+  # I added the argument check.names to allow for disabling checking for validity of coefficient names, because it may not matter if I use the resulting contrast matrix for other purposes/in other packages;
+  # otherwise, if a design matrix from model.matrix is passed to contrasts, interaction terms like a:b would be invalid and cause this function to stop,
+
+	e <- substitute(list(...))
+	if(is.factor(levels)) levels <- levels(levels)
+	if(!is.character(levels)) levels <- colnames(levels)
+	if(levels[1]=="(Intercept)") {
+		levels[1] <- "Intercept"
+		warning("Renaming (Intercept) to Intercept")
+	}
+  if (check.names) {
+	  notvalid <- (levels != make.names(levels))
+	  if(any(notvalid)) stop("The levels must by syntactically valid names in R, see help(make.names).  Non-valid names: ",paste(levels[notvalid],collapse=","))
+  }
+	n <- length(levels)
+	if(n < 1) stop("No levels to construct contrasts from")
+	indicator <- function(i,n) {
+		out <- rep(0,n)
+		out[i] <- 1
+		out
+	}
+	levelsenv <- new.env()
+	for (i in 1:n) assign(levels[i], indicator(i,n), pos=levelsenv)
+
+#	Contrasts given as character vector
+	if(!is.null(contrasts)) {
+		if(length(e)>1) stop("Can't specify both ... and contrasts")
+		e <- as.character(contrasts)
+		ne <- length(e)
+		cm <- matrix(0,n,ne, dimnames=list(Levels=levels,Contrasts=e))
+		if(ne==0) return(cm)
+		for (j in 1:ne) {
+			ej <- parse(text=e[j])
+			cm[,j] <- eval(ej, envir=levelsenv)
+		}
+		return(cm)
+	}
+
+#	Contrasts given as list of expressions
+	ne <- length(e)
+	enames <- names(e)[2:ne]
+	easchar <- as.character(e)[2:ne]
+	if(is.null(enames))
+		cn <- easchar
+	else
+		cn <- ifelse(enames=="",easchar,enames)
+	cm <- matrix(0,n,ne-1, dimnames=list(Levels=levels,Contrasts=cn))
+	if(ne < 2) return(cm)
+	for (j in 1:(ne-1)) {
+		ej <- e[[j+1]]
+		if(is.character(ej)) ej <- parse(text=ej)
+		ej <- eval(ej, envir=levelsenv)
+#		Character variable
+		if(!is.numeric(ej)) {
+			colnames(cm)[j] <- as.character(ej)[1]
+			if(is.character(ej)) ej <- parse(text=ej)
+			ej <- eval(ej, envir=levelsenv)
+		}
+		cm[,j] <- ej
+	}
+	cm
+}
+
+
